79c83056d42e0431d8250f4a8d11bfb201f2bd90
Seven <lixfeng.3@gmail.com>
2013-03-27 17:57:16 +0800

    view utils

--- src/info/ishared/android/MainActivity.java
+++ src/info/ishared/android/MainActivity.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import com.github.rtyley.android.sherlock.roboguice.activity.RoboSherlockActivity;
 import info.ishared.android.util.ToastUtils;
+import info.ishared.android.util.ViewUtils;
 import roboguice.inject.InjectView;
 
 public class MainActivity extends RoboSherlockActivity {
     @InjectView(R.id.run_btn) private Button mRunButton;
     @InjectView(R.id.stop_btn) private Button mStopButton;
     @InjectView(R.id.setting_btn) private Button mSettingButton;
+    @InjectView(R.id.pb_loading) private ProgressBar mLoading;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,12 +31,14 @@ public class MainActivity extends RoboSherlockActivity {
             @Override
             public void onClick(View view) {
                 showMessage("stop click");
+                ViewUtils.setGone(mLoading, true);
             }
         });
         mSettingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showMessage("setting click");
+                ViewUtils.setGone(mLoading, false);
             }
         });
     }
